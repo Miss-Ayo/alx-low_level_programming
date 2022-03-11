6dba81b390a11c08206bebe27a6f9e548ba3099c
Miss-Ayo <behanzinlaurencia@gmail.com>
2022-03-10 17:11:57 -0800

    update

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,6 @@
 /**
- * main - main function
+ * main - print the size of some type
+ * Description : program that prints the size of various types on the computer
  * Return : Always 0 (sucess)
 */
 
@@ -12,5 +13,5 @@ printf("Size of an int: %i byte(s)\n", sizeof(int));
 printf("Size of a long int: %i byte(s)\n", sizeof(long int));
 printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
 printf("Size of a float: %i byte(s)\n", sizeof(float));
-  return (0);
+return (0);
 }
