9ffa21c104fb8385223468f35ab4465ce1cb68b1
Miss-Ayo <behanzinlaurencia@gmail.com>
2022-03-10 16:31:11 -0800

    upload

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 /**
- *main
- *main function
+ * main - main block
+ * Return : 0
 */
 
 #include <stdio.h>
 
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle\n");
-  return (0);
+puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 /**
- * printf function
- * printf
+ * main - main block
+ * Return : 0
 */
 
 #include <stdio.h>
 
 int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art,\n");
-  return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
