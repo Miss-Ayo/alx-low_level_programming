95eb2290c2cbb8622611cd211d31f891d13f03b0
Miss-Ayo <behanzinlaurencia@gmail.com>
2022-03-14 14:57:43 -0700

    update

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+/**
+ * main - print alphabet in lowercase
+ * Return: 0 successfully
+ */
+
+int main(void)
+{
+char A = 'a';
+ 
+while (A <= 'z')
+{
+putchar(A);
+A++;
+}
+putchar('\n');
+return (0);
+}
