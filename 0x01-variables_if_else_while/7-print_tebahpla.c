978c46f0560c68670eb74048e80b53f0df04131a
Miss-Ayo <behanzinlaurencia@gmail.com>
2022-03-14 17:19:19 -0700

    update function

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+/**
+ * main - alphabetic in reverse
+ * Return: 0 successfully
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+char A = 'z';
+
+while (A >= 'a')
+{
+putchar(A);
+A--;
+}
+putchar('\n');
+return (0)
+}
