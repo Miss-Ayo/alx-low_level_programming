3180631689efaab76805398a5e9eb7e5d47fb7cb
laurencia behanzin <laurenciabehanzin22@gmail.com>
2022-03-16 01:23:30 +0100

    print alpahabet in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+/**
+ * main - prins the alpahabet in lowercase
+ * Return: 0 successfully
+ */
+
+#include "main.h"
+
+void print_alphabet(void)
+{
+char character = 'a';
+
+while (character <= 'z') {
+putchar(character);
+}
+putchar('\n');
+}
+
+int main(void)
+{
+print_alphabet();
+return (0);
+}
