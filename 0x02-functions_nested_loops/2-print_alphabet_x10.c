e30712e6cd76d6253eb43a0a3b0525f12a1b1f86
laurencia behanzin <laurenciabehanzin22@gmail.com>
2022-03-17 01:49:38 +0100

    print alphabet in lowercase 10 x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,14 @@
+/**
+ * print_alphabet_x10 - print the alphabet in lowercase 10 times
+ * Return: 0 successfully
+ */
+
+#include "main.h"
+
+void print_alphabet_x10(void)
+{
+for (int i = 1; i <= 10; i++)
+{
+print_alphabet();
+}
+}
