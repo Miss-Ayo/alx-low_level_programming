2b565bf27c831180ae3c8e5ba1993c0d2c5ff02d
laurencia behanzin <laurenciabehanzin22@gmail.com>
2022-03-17 15:55:01 +0100

    print numbers function

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+/**
+ * print_most_numbers - print numbers from 0 to 9 except 2 and 4
+ * Return: void
+ */
+
+#include "main.h"
+
+void print_most_numbers(void)
+{
+int i = '0';
+
+while (i <= '9')
+{
+if ( i != '2' && i != '4')
+_putchar(i);
+i++;
+}
+putchar('\n');
+}
