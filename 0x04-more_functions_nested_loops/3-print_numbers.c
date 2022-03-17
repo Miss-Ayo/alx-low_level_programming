bd900dd3ca83b053e0fcecb5c78cadccad74babf
laurencia behanzin <laurenciabehanzin22@gmail.com>
2022-03-17 15:21:06 +0100

    print numbers function

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+/**
+ * print_numners - print number from 0 to 9
+ * Return: 0
+ */
+
+#include "main.h"
+
+void print_numbers(void)
+{
+int i = 0;
+
+while (i <= 9) {
+_putchar(i);
+i++;
+}
+_putchar('\n');
+}
