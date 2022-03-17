f7e32adca4184bbeadef8c5277446b03d7a71763
laurencia behanzin <laurenciabehanzin22@gmail.com>
2022-03-17 01:39:51 +0100

    update

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,5 +1,7 @@
 /**
  * main - prins the alpahabet in lowercase
+ * Description: print alphabet in lowercase, followed
+ * by a newline
  * Return: 0 successfully
  */
 
@@ -10,7 +12,8 @@ void print_alphabet(void)
 {
 char character = 'a';
 
-while (character <= 'z') {
+while (character <= 'z')
+{
 putchar(character);
 character++;
 }
