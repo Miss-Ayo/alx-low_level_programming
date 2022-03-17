aac9d22f44cfe00a7019652cd23c7626533f9092
laurencia behanzin <laurenciabehanzin22@gmail.com>
2022-03-17 02:49:57 +0100

    check whether c is a character or not

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+/**
+ * _isalpha - checks for alphabetic character. 
+ * @c: the character to be checked
+ * Return: 1 if c is a letter and 0 otherwise
+ */
+
+#include "main.h"
+
+int _isalpha(int c)
+{
+if (c >= 65 && c <= 90 || c >= 97 && c <= 122)
+return (1);
+else
+return (0);   
+}
