e57cc6f5a81b93d7099f1a570d431ad027239c8a
laurencia behanzin <laurenciabehanzin22@gmail.com>
2022-03-17 02:40:39 +0100

    check wether a character is lowercase or not

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -12,5 +12,5 @@ if (c >= 97 && c <= 122 )
 return (1);
 else
 return (0);
-
 }
+
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,6 +9,7 @@
 #include <stdio.h>
 #include <unistd.h>
 
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
