f3c39a0c7ad3ba91df12ee91e97aacefd9646469
laurencia behanzin <laurenciabehanzin22@gmail.com>
2022-03-30 00:53:47 +0100

    memcpy + strchr

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,18 +9,15 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-unsigned int var1 = 0;
-unsigned int var2 = 0;
+unsigned int var1, var2;
 
-while (*(s + var2))
+for (var2 = 0; *(s + var2); var2++)
 {
-while (*(accept + var1))
+for (var1 = 0; *(accept + var1); var1++)
 {
 if (*(s + var2) == *(accept + var1))
 break;
-var1++;
 }
-var2++;
 if (*(accept + var1) == '\0')
 break;
 }
