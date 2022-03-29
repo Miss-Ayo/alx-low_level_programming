2e69d346ac72f10f179d983d720d41508cddac5f
laurencia behanzin <laurenciabehanzin22@gmail.com>
2022-03-30 00:56:53 +0100

    memcpy + strchr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,13 @@
+/**
+ * _strstr - locates a substring
+ * @haystack: the string
+ * @needle: the searching string
+ * Return: new string.
+ */
+
+#include "main.h"
+
+char *_strstr(char *haystack, char *needle)
+{
+return (0);
+}
