8c89159d08e56511dde98feaa9eb97ef82d705bc
laurencia behanzin <laurenciabehanzin22@gmail.com>
2022-03-31 01:45:31 +0100

    updates

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,13 @@
+/**
+ * is_prime_number - verify if a number is prime
+ * @n: the integer to be checked
+ *
+ * Return: prime number
+ */
+
+#include "main.h"
+
+int is_prime_number(int n)
+{
+return (is_prime_number(n));
+}
