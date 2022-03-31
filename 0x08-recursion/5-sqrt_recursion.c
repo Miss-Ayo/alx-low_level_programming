7bd1702e65ac0c1db09d9d08d8cabc801dff02fa
laurencia behanzin <laurenciabehanzin22@gmail.com>
2022-03-31 01:42:00 +0100

    updates

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,18 +1,18 @@
 /**
- * sqrt - evaluate from 1 to n
+ * _square - evaluate from 1 to n
  * @a: a equal to n
  * @b: itinerate 1 to the number n
  *
  * Return: the square  root
  */
 
-int sqrt(int a, int b)
+int _square(int a, int b)
 {
 if (b * b == a)
 return (b);
 else if (b * b > a)
 return (-1);
-return (sqrt(a, b + 1));
+return (_square(a, b + 1));
 }
 
 /**
@@ -26,5 +26,5 @@ return (sqrt(a, b + 1));
 
 int _sqrt_recursion(int n)
 {
-return (sqrt(n, 1));
+return (_square(n, 1));
 }
