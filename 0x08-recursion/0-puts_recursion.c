345bf9638a946fce2403bf72b576ba37e4a84510
laurencia behanzin <laurenciabehanzin22@gmail.com>
2022-03-31 01:09:20 +0100

    puts recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+/**
+ * _puts_recursion - prints a string, followed by a new line
+ * @s: the string to print
+ * 
+ * Return: a void
+ */
+
+void _puts_recursion(char *s)
+{
+if (*s != 0)
+{
+_putchar(*s);
+_puts_recursion(s + 1);
+}
+_putchar('\n');
+return;
+}
