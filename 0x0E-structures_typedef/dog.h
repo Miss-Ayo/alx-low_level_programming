d4302faa570141f5a6bbb76bd27aac7ebcfcbe8e
laurencia behanzin <laurenciabehanzin22@gmail.com>
2022-04-13 00:32:44 +0100

    init struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,4 +15,8 @@ char *owner;
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+/**
+ * dog - rename struct
+ */
+typedef struct dog dog_t;
 #endif
