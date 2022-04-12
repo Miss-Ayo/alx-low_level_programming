89c82c4aa5cf4d60d118554923b0abaa1d056df5
laurencia behanzin <laurenciabehanzin22@gmail.com>
2022-04-12 23:45:28 +0100

    declare struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - list dates for pets
+ * @name: pet name
+ * @owner: pet owner name
+ * @age: pet age
+ */
+
+struct dog
+{
+    char *owner;
+    float age;
+    char *name;
+};
+
+
+#endif
