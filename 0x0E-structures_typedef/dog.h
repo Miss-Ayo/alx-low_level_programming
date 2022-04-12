f2b423a73c9287b1636aee3de702b9bb02b3e8b6
laurencia behanzin <laurenciabehanzin22@gmail.com>
2022-04-12 23:58:33 +0100

    declare struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -11,11 +11,7 @@
 struct dog
 {
 char *owner;
-float age;
 char *name;
+float age;
 };
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-
 #endif
