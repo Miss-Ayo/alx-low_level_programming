85f1837e94571ea4763fbc6499069dec5211ab1f
laurencia behanzin <laurenciabehanzin22@gmail.com>
2022-04-13 00:18:26 +0100

    init struct

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdlib.h>
 
 /**
  * init_dog - init struct
@@ -6,6 +7,7 @@
  * @name: store a string
  * @age: store a number
  * @owner: store a string
+ *
  * Return: void
  */
 
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,3 @@
-#include <stdlib.h>
-
 #ifndef DOG_H
 #define DOG_H
 
