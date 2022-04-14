69b0cde7e2158c7b054b468f13bfb06cffb95a0d
laurencia behanzin <laurenciabehanzin22@gmail.com>
2022-04-14 02:08:44 +0100

    UPDATING function

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -6,7 +6,6 @@
 */
 
 #include <stdio.h>
-#include <stdlib.h>
 #include "function_pointers.h"
 
 void print_name(char *name, void (*f)(char *))
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,5 @@
-#ifndef POINTERS_H
-#define POINTERS_H
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
 
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
