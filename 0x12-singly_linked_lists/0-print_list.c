940867d52f460ad0941926ffbaaa4c37dd419554
laurencia behanzin <laurenciabehanzin22@gmail.com>
2022-04-21 22:29:31 +0100

    update

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,7 +10,7 @@ size_t print_list(const list_t *h)
 {
 size_t count = 0;
 
-while (h != NULL)
+while (h)
 {
 if (h->str == NULL)
 printf("[0] (nil)\n");
---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * addd_node - adds a new node at the beginning of a list
+ * @head: head of a list
+ * @str : element of a list
+ * Return : the address of the new element, or NULL if it failed
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+list_t *nodes = malloc(sizeof(list_t));
+char *nodes_str = strdup(str);
+unsigned int nodes_len = 0;
+
+if (!nodes_str)
+return (NULL);
+if (nodes == NULL)
+{
+free(nodes_str);
+return (NULL);
+}
+while (nodes_str[nodes_len] != '\0')
+nodes_len++;
+nodes->str = nodes_str;
+nodes->len = nodes_len;
+nodes->next = *head;
+*head = nodes_str;
+return (nodes);
+}
