ccbfd8b30d5ff4ac69ce5463883490e7e1ee0a9b
laurencia behanzin <laurenciabehanzin22@gmail.com>
2022-04-22 00:29:26 +0100

    update

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -9,23 +9,22 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-list_t *nodes = NULL;
+list_t *nodes = malloc(sizeof(list_t));
 char *nodes_str = strdup(str);
 unsigned int nodes_len = 0;
 
-if (!nodes_str)
-return (NULL);
-while (nodes_str[nodes_len] != '\0')
-nodes_len++;
-nodes = malloc(sizeof(list_t));
 if (nodes == NULL)
+return (NULL);
+if (nodes_str == NULL)
 {
-free(nodes_str);
+free(nodes);
 return (NULL);
 }
+while (nodes_str[nodes_len] != '\0')
+nodes_len++;
 nodes->str = nodes_str;
 nodes->len = nodes_len;
 nodes->next = *head;
-*head = nodes_str;
+*head = nodes;
 return (nodes);
 }
