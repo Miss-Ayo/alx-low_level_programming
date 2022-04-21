93d7991b072ffa4dd8dba7e23d64f9f18f53fb28
laurencia behanzin <laurenciabehanzin22@gmail.com>
2022-04-21 18:15:30 +0100

    update

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,16 +8,16 @@
 
 size_t print_list(const list_t *h)
 {
-size_t count;
+    size_t count;
 
-while (h)
-{
-if (h->str == NULL)
-printf("[0] (nil)");
-else if (h->str != NULL)
-printf("[%u] %s", h->len, h->str);
-count++;
-h = h->next;
-}
-return (count);
+    while (h)
+    {
+        if (h->str == NULL)
+            printf("[0] (nil)");
+        else if (h->str != NULL)
+            printf("[%u] %s", h->len, h->str);
+        count++;
+        h = h->next;
+    }
+    return (count);
 }
